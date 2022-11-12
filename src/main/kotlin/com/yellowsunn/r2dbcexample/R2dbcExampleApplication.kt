3cbeb50fe7056e2a7eff62e068c63dcc2c75ee90
yellowsunn <chohankook95@gmail.com>
2022-11-12 15:44:45 +0900

    init project

---
+++ src/main/kotlin/com/yellowsunn/r2dbcexample/R2dbcExampleApplication.kt
@@ -0,0 +1,11 @@
+package com.yellowsunn.r2dbcexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class R2dbcExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<R2dbcExampleApplication>(*args)
+}
---
+++ src/test/kotlin/com/yellowsunn/r2dbcexample/R2dbcExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yellowsunn.r2dbcexample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class R2dbcExampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
